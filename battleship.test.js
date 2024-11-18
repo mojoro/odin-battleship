b0a069592ed7a22840f105f52a1e067ae0a0479a
John Moorman <45500240+mojoro@users.noreply.github.com>
2024-11-18 22:33:11 +0100

    Finish ship class and add first Gameboard test

--- battleship.test.js
+++ battleship.test.js
@@ -1,7 +1,18 @@
 import { Ship } from "./ship";
+import { Gameboard } from "./gameboard";
 
 test("Ship sinks after fully hit", () => {
   const testShip = new Ship(5);
   testShip.hit().hit().hit().hit().hit();
   expect(testShip.isSunk()).toBeTruthy();
+});
+
+test("Ship is not sunk initially", () => {
+  const testShip = new Ship(5);
+  expect(testShip.isSunk()).toBeFalsy();
+});
+
+test("Gameboard is 10x10", () => {
+  const testBoard = new Gameboard();
+  expect(testBoard.board.length && testBoard.board[0].length).toBe(10);
 });
--- ship.js
+++ ship.js
@@ -4,6 +4,17 @@ class Ship {
     this.hits = 0;
     this.sunk = false;
   }
+
+  hit() {
+    this.hits++;
+    return this;
+  }
+
+  isSunk() {
+    if (this.hits >= this.size) {
+      return true;
+    } else return false;
+  }
 }
 
 export { Ship };
